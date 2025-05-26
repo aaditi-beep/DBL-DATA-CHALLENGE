23553451df7f0399ce3b3d9c3c16f61ad0660efa
Stylianos Kasapakis <stylianos.k.kasapakis@gmail.com>
2025-05-26 23:32:31 +0000

    Upload New File

---
+++ airline_analysis.py
@@ -0,0 +1,99 @@
+from pymongo import MongoClient
+from collections import defaultdict
+import pandas as pd
+from datetime import datetime
+
+# MongoDB connection
+client = MongoClient("mongodb://localhost:27017")
+db = client[""]
+tweets = db[""]
+
+
+
+# List of airlines you care about
+airlines = [
+    "KLM", "AirFrance", "British_Airways", "AmericanAir", "Lufthansa", "AirBerlin",
+    "AirBerlin assist", "easyJet", "RyanAir", "SingaporeAir", "Qantas",
+    "EtihadAirways", "VirginAtlantic"
+]
+
+pipeline_tweet_counts = [
+    {"$match": {"airline": {"$in": airlines}}},
+    {"$group": {"_id": "$airline", "tweet_count": {"$sum": 1}}}
+]
+
+tweet_counts = list(tweets.aggregate(pipeline_tweet_counts))
+print("Tweet counts per airline:", tweet_counts)
+
+pipeline_avg_convo_len = [
+    {"$match": {"airline": {"$in": airlines}}},
+    {"$group": {
+        "_id": {"airline": "$airline", "conversation_id": "$conversation_id"},
+        "convo_size": {"$sum": 1}
+    }},
+    {"$group": {
+        "_id": "$_id.airline",
+        "avg_convo_length": {"$avg": "$convo_size"}
+    }}
+]
+
+
+result = conversations.delete_many({ "airline": None })
+
+avg_convo_lengths = list(tweets.aggregate(pipeline_avg_convo_len))
+print("Average conversation lengths per airline:", avg_convo_lengths)
+
+pipeline_convos_per_month = [
+    {"$match": {"airline": {"$in": airlines}}},
+    {"$group": {
+        "_id": {
+            "airline": "$airline",
+            "conversation_id": "$conversation_id",
+            "year_month": {
+                "$dateToString": {"format": "%Y-%m", "date": "$created_at"}
+            }
+        }
+    }},
+    {"$group": {
+        "_id": {
+            "airline": "$_id.airline",
+            "year_month": "$_id.year_month"
+        },
+        "num_convos": {"$sum": 1}
+    }},
+    {"$sort": {"_id.year_month": 1}}
+]
+
+pipeline = [
+    {
+        "$lookup": {
+            "from": "tweets",
+            "localField": "root_tweet",
+            "foreignField": "_id",
+            "as": "root_info"
+        }
+    },
+    {
+        "$unwind": "$root_info"
+    },
+    {
+        "$group": {
+            "_id": "$root_info.lang",
+            "count": { "$sum": 1 }
+        }
+    },
+    {
+        "$sort": { "count": -1 }
+    }
+]
+
+results = list(conversations.aggregate(pipeline))
+print("Languages by conversation count:")
+for result in results:
+    print(f"{result['_id']}: {result['count']}")
+
+convos_per_month = list(tweets.aggregate(pipeline_convos_per_month))
+df = pd.DataFrame(convos_per_month)
+df["_id"] = df["_id"].apply(lambda x: (x["airline"], x["year_month"]))
+df.columns = ["(Airline, Month)", "Conversation Count"]
+print(df)
